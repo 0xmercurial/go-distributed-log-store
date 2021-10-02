9611d8dc77809378b998ed21860b1e083e52624c
RobertJacksonIO <jackson.t.robert@gmail.com>
2021-10-01 19:51:59 -0600

    Adding tests for index

--- logpack/index_test.go
+++ logpack/index_test.go
@@ -1,1 +1,37 @@
 package logpack
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndex(t *testing.T) {
+	f, err := ioutil.TempFile(os.TempDir(), "index_test")
+	assert.Equal(t, err, nil)
+	defer os.Remove(f.Name())
+
+	c := Config{}
+	c.Segment.MaxIndexBytes = 1024
+	idx, err := newIndex(f, c)
+	assert.Equal(t, err, nil)
+
+	_, _, err = idx.Read(-1)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, f.Name(), idx.Name())
+
+	entries := []struct {
+		Off uint32
+		Pos uint64
+	}{
+		{Off: 0, Pos: 0},
+		{Off: 1, Pos: 10},
+	}
+
+	for _, want := range entries {
+		err = idx.Write(want.Off, want.Pos)
+		assert.Equal(t, err, nil)
+	}
+}
