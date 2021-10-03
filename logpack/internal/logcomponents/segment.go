21f52472529fc77e9787136bc3a34f338f3da2d7
RobertJacksonIO <jackson.t.robert@gmail.com>
2021-10-03 15:06:35 -0600

    Add read method for segment

--- logpack/internal/logcomponents/segment.go
+++ logpack/internal/logcomponents/segment.go
@@ -2,8 +2,11 @@ package logcomponents
 
 import (
 	"fmt"
+	prolog "logpack/internal/log/proto"
 	"os"
 	"path"
+
+	"google.golang.org/protobuf/proto"
 )
 
 type segment struct {
@@ -43,19 +46,51 @@ func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
 	if s.index, err = newIndex(indexFile, c); err != nil {
 		return nil, err
 	}
+	//Check if there are any entries via index
 	if off, _, err := s.index.Read(-1); err != nil {
 		s.nextOffset = baseOffset
 	} else {
+		//If records exist, set offset to the position of the next entry
 		s.nextOffset = baseOffset + uint64(off) + 1
 	}
 	return s, nil
 }
 
-// func (s *segment) Append(record *prolog.Record) (offset uint64, err error) {
-// 	cur := s.nextOffset
-// 	record.Offset = cur
-// 	p, err := proto.Marshal(record)
-// 	if err != nil {
+func (s *segment) Append(record *prolog.Record) (offset uint64, err error) {
+	cur := s.nextOffset
+	record.Offset = cur
+	//Marshal protobuf message
+	p, err := proto.Marshal(record)
+	if err != nil {
+		return 0, err
+	}
+	//Write actual record to store
+	_, pos, err := s.store.Append(p)
+	if err != nil {
+		return 0, err
+	}
+	//Update index
+	if err = s.index.Write(
+		uint32(s.nextOffset-uint64(s.baseOffset)),
+		pos,
+	); err != nil {
+		return 0, err
+	}
+	s.nextOffset++
+	return cur, nil
+}
 
-// 	}
-// }
+func (s *segment) Read(off uint64) (*prolog.Record, error) {
+	_, pos, err := s.index.Read(int64(off - s.baseOffset))
+	if err != nil {
+		return nil, err
+	}
+	p, err := s.store.Read(pos)
+	if err != nil {
+		return nil, err
+	}
+
+	record := &prolog.Record{}
+	err = proto.Unmarshal(p, record)
+	return record, err
+}
