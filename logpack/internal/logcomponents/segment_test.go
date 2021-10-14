6fe1331c22aa4b4eda3c1e1099815b393fd8a25c
RobertJacksonIO <jackson.t.robert@gmail.com>
2021-10-14 15:13:53 -0600

    test[internal/logcomponents/segment_test.go]: Updating tests.

--- logpack/internal/logcomponents/segment_test.go
+++ logpack/internal/logcomponents/segment_test.go
@@ -5,15 +5,27 @@ import (
 	prolog "logpack/internal/log/proto"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSegment(*testing.T) {
+func TestSegment(t *testing.T) {
 	dir, _ := ioutil.TempDir("", "seg-test")
 	defer os.Remove(dir)
 
 	want := &prolog.Record{Value: []byte("hello world")}
+	t.Log(want)
 
 	c := Config{}
 	c.Segment.MaxStoreBytes = 1024
 	c.Segment.MaxIndexBytes = entWidth * 3
+
+	s, err := newSegment(dir, 16, c)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, uint64(16), s.nextOffset)
+	assert.Equal(t, false, s.IsMaxed())
+
+	for i := uint64(0); i < 3; i++ {
+	}
+
 }
