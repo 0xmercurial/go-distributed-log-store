2b7c4237146aaee5e6abb9ecf8ef524d9eb154e7
RobertJacksonIO <jackson.t.robert@gmail.com>
2021-12-26 11:43:00 -0700

    feat[internal/replica.go]: Finishinhg implementing node replication.

--- internal/logcomponents/replica.go
+++ internal/logcomponents/replica.go
@@ -1,6 +1,7 @@
 package logcomponents
 
 import (
+	"context"
 	"logstore/internal/log/proto"
 	"sync"
 
@@ -33,6 +34,55 @@ func (r *Replica) init() {
 
 func (r *Replica) replicate(addr string, leave chan struct{}) {
 	clientConn, err := grpc.Dial(addr, r.DialOptions...)
+	if err != nil {
+		r.logError(err, "failed to dial", addr)
+		return
+	}
+	defer clientConn.Close()
+
+	client := proto.NewLogClient(clientConn)
+
+	ctx := context.Background()
+	stream, err := client.ReadStream(
+		ctx,
+		&proto.ReadRequest{Offset: 0},
+	)
+	if err != nil {
+		r.logError(err, "failed to read", addr)
+		return
+	}
+
+	records := make(chan *proto.Record)
+	go func() {
+		for {
+			recv, err := stream.Recv()
+			if err != nil {
+				r.logError(err, "failed to receive msg", addr)
+				return
+			}
+			records <- recv.Record
+		}
+	}()
+
+	for {
+		select {
+		case <-r.close:
+			return
+		case <-leave:
+			return
+		case record := <-records:
+			_, err = r.LocalServer.Append(
+				ctx,
+				&proto.AppendRequest{
+					Record: record,
+				},
+			)
+			if err != nil {
+				r.logError(err, "failed to append", addr)
+				return
+			}
+		}
+	}
 }
 
 func (r *Replica) Join(name, addr string) error {
@@ -51,4 +101,38 @@ func (r *Replica) Join(name, addr string) error {
 	go r.replicate(addr, r.servers[name])
 
 	return nil
+}
+
+func (r *Replica) Leave(name string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.init()
+
+	if _, ok := r.servers[name]; !ok {
+		return nil
+	}
+	close(r.servers[name])
+	delete(r.servers, name)
+	return nil
+}
+
+func (r *Replica) Close() error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.init()
+
+	if r.closed {
+		return nil
+	}
+	r.closed = true
+	close(r.close)
+	return nil
+}
+
+func (r *Replica) logError(err error, msg, addr string) {
+	r.logger.Error(
+		msg,
+		zap.String("addr", addr),
+		zap.Error(err),
+	)
 }
