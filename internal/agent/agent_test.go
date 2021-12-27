bda56b42bda11993fb7fb3ce63638d0864245300
RobertJacksonIO <jackson.t.robert@gmail.com>
2021-12-27 11:46:45 -0700

    test[internal/agent/agent_test.go]: starting agent tests.

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -3,14 +3,35 @@ package agent
 import (
 	"logstore/internal/config"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAgent(t *testing.T) {
 	serverTLSConfig, err := config.SetupFromTLSConfig(
 		config.TLSConfig{
-			CertFile: config.ServerCertFile,
-			KeyFile:  config.ServerKeyFile,
-			CAFile:   config.CAFile,
+			CertFile:      config.ServerCertFile,
+			KeyFile:       config.ServerKeyFile,
+			CAFile:        config.CAFile,
+			Server:        true,
+			ServerAddress: "127.0.0.1",
 		},
 	)
+	assert.NoError(t, err)
+
+	peerTLSConfig, err := config.SetupFromTLSConfig(
+		config.TLSConfig{
+			CertFile:      config.RootClientCertFile,
+			KeyFile:       config.RootClientKeyFile,
+			CAFile:        config.CAFile,
+			Server:        false,
+			ServerAddress: "127.0.0.1",
+		},
+	)
+	assert.NoError(t, err)
+
+	var agents []*Agent
+	for i := 0; i < 3; i++ {
+
+	}
 }
